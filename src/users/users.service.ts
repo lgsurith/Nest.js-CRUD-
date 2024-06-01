3ecc0955d5b77db2a2b019e5a11d7b37547e3ad0
Surith <surithcodes204@gmail.com>
2024-06-01 18:56:30 +0530

    Error Handling - User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 
 //we will be implying an async and await
 //basic crud operations are done here !.
@@ -17,22 +18,39 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto) {
-    const user = this.usersRepositry.create(createUserDto);
+    try{
+    const hashedPassword = await bcrypt.hash(createUserDto.password , 10);
+    const user = this.usersRepositry.create({...createUserDto , password : hashedPassword});
     return await this.usersRepositry.save(user);
+    }catch(error){
+      console.error('User cannot be created : ' , error);
+      throw error;
+    }
   }
 
   async findAll() {
+    try{
     return await this.usersRepositry.find();
+    }catch(error){
+      console.error('Cannot fetch all the users : ' , error);
+      throw error;
+    }
   }
 
 
   async findOne(id: number) {
+    try{
     return await this.usersRepositry.findOne({
       where : { id }
     });
+    }catch(error){
+      console.error('Cannot find User : ' , error);
+      throw error;
+    }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
+    try{
     const user = await this.findOne(id);
 
     if(!user){
@@ -40,13 +58,22 @@ export class UsersService {
     }
     Object.assign(user , updateUserDto);
     return await this.usersRepositry.save(user);
+    }catch(error){
+      console.error('Cannot Update User : ' , error);
+      throw error;
+    }
   }
 
   async remove(id: number) {
+    try{
     const user = await this.findOne(id);
     if(!user){
       throw new NotFoundException();
     }
     return await this.usersRepositry.remove(user);
+    }catch(error){
+      console.error('Cannot remove the user : ' , error);
+      throw error;
+    }
   }
 }
